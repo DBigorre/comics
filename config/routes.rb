c88f8e81f4029df09b99d24875d81868b06676cc
desiree bigorre <bigorredesiree@gmail.com>
2022-06-22 15:11:34 -0400

    model et seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  root to: 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "comics/dc", to: "comics#indexdc"
+  get "comics/marvel", to: "comics#indexmarvel"
+  get "comics/valiant", to: "comics#indexvaliant"
+  get "comics/other", to: "comics#indexother"
+  resources :comics
 end
